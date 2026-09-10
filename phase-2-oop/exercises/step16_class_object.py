93db85ca6032f390a1e56eccbb1fdc4008e6a387
Noman <322531288+noman-agentic@users.noreply.github.com>
2026-09-10 23:24:32 +0600

    Add Phase 2 Step 16: Class and Object basics

---
+++ phase-2-oop/exercises/step16_class_object.py
@@ -0,0 +1,29 @@
+class Robot:
+    pass
+
+
+robot1 = Robot()
+robot2 = Robot()
+
+robot1.name = "Lead Collector Robot"
+robot2.name = "Customer Support Robot"
+
+print(robot1.name)
+print(robot2.name)
+
+
+class Task:
+    pass
+
+task1 = Task()
+task2 = Task()
+task3 = Task()
+
+task1.status = "pending"
+task2.status = "completed"
+task3.status = "failed"
+
+print(task1.status)
+print(task2.status)
+print(task3.status)
+
